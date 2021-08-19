f4212a3ed9a0a349aa66b52334d215d6694de3b9
wesleivisentin <wesleivisentin@hotmail.com>
2021-08-19 19:10:58 -0300

    jogo_da_velha2
    2

--- README.py
+++ README.py
@@ -1,1 +1,37 @@
-print ('olá mundo')
+palavra= input('digite a palavra secreta:').lower().strip()
+for x in range(20):
+    print()
+digitadas= []
+acertos= []
+erros= 0
+while True:
+    senha= ""
+    for letra in palavra:
+        senha += letra if letra in acertos else "." 
+    print(senha)
+    if senha == palavra:
+        print('você acertou!')
+        break
+    tentativa= input('\nDigite uma letra:').lower().strip()
+    if tentativa in digitadas:
+        print('Você já tentou esta letra!')
+        continue
+    else:
+        digitadas += tentativa
+        if tentativa in palavra:
+            acertos += tentativa
+        else:
+            erros += 1
+            print('Você errou!')
+    print('x==:==\nx : ') 
+    print('x O ' if erros >= 1 else 'x')
+    linha2= ''
+    if erros == 2:
+        linha2=' | '
+    elif erros == 3:
+        linha2= ' \| ' 
+    elif erros >= 4:
+        linha2=' \|/ '
+    print(f'X{linha2}')
+    linha3=''
+               
